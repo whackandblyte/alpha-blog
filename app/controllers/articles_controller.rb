dab146da0319a6d0bf1026f07532e3c285075f71
Henrik Wollersheim <henrik@wollersheim.org>
2017-01-26 22:30:11 +0100

    Added articles index listing, edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @all_articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -16,7 +20,21 @@ class ArticlesController < ApplicationController
     #redirect_to articles_path(@article)
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
     @article = Article.find(params[:id])
   end
 

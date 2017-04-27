074845d75e24705516539fe1d2b6c01bf468e7cb
Henrik Wollersheim <henrik@wollersheim.org>
2017-04-27 22:28:14 +0200

    Added category editing (only for admins)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,13 +24,24 @@ class CategoriesController < ApplicationController
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category successfully renamed."
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def require_admin
     if !logged_in? || (logged_in? && !current_user.admin?)
-      flash[:danger] = "Only admins can create new categories."
+      flash[:danger] = "Only admins can create or update new categories."
       redirect_to categories_path
     end
   end
